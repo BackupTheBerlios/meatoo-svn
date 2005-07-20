838d4bf0089a29afeb19bafa2aef53e3028d6cc1
pythonhead <pythonhead@636757c4-2ffc-0310-b323-e4f50545bae7>
2005-07-20 07:13:56 +0000

    New header with meatoo.png

--- trunk/sections.py
+++ trunk/sections.py
@@ -8,34 +8,32 @@ def header():
     logged = False
     if cherrypy.session.get('userid', None):
         logged = True
+
     template = Template("""
     <html><head><title>Meatoo: Freshmeat-Gentoo Updates</title>
       <link href="/meatoo/static/stylin.css" media="screen" rel="Stylesheet" type="text/css" />
       <link href="http://gentooexperimental.org/meatoo/static/meatoo.xml" rel="alternate" type="application/rss+xml" title="Meatoo Freshmeat-Gentoo RSS Feed" />
     </head>
     <body>
+    <table cellspacing=0 cellpadding=0 border=0>
+    <tr border=0><td border=0><a href="/meatoo"><img border=0 src="/meatoo/static/meatoo.png"></a></td>
 
-    <h1><a href="/meatoo">Meatoo</a></h1>
-    <br><br>
+    <td border=0><td rowspan="2" colspan="2"><a href="/meatoo/static/faq.html">FAQ</a> -Advanced Search -
     #if $logged:
-        <a href="/meatoo/logout">Logout</a>
+        <a href="/meatoo/logout">Logout</a> -
+        <a href="/meatoo/options">Options</a>
     #else
         <a href="/meatoo/login">Login</a>
     #end if
-    <br><br>
-    Meatoo is a database of the latest Freshmeat releases having name matches
-    in Gentoo's portage.<br>
-    The database is updated twice a day at approximately 12am and 12pm UTC<br>
+    </td>
+    </tr>
+    <tr><td colspan="2"><b>Find Gentoo packages needing a version bump by checking the latest Freshmeat releases.</b></td></tr>
+    </table>
 
-    <br>
-    Please read the <a href="/meatoo/static/faq.html">FAQ</a> for more info.
-    <br>
-    <br>
-    <br>
     <table>
     <tr>
         <td>Search by Herd/Maintainer
-            <form name='search' action="/meatoo/search" method=post>
+            <form class="search" id="search" name='search' action="/meatoo/search" method=post>
             <input type='input' name='srch' value="">
             <input type='hidden' name='type' value="herd">
 
